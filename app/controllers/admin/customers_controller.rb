eef3e55f7b47eb5228985cdabb2d00780f4f3f3a
kao126 <76607249+kao126@users.noreply.github.com>
2022-05-17 10:05:52 +0000

    [Add]会員詳細画面（管理者）

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,4 +3,8 @@ class Admin::CustomersController < ApplicationController
     @customers = Customer.all
   end
 
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,8 +5,14 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :addresses, dependent: :destroy
 
+  enum is_deleted: { "有効": false, "退会": true }
+
   def full_name
     self.last_name + self.first_name
+  end
+
+  def full_name_kana
+    self.last_name_kana + self.first_name_kana
   end
 
 end
