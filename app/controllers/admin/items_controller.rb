2521ca98d185192555615e7cdffa56326bd0a340
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-05-17 13:14:00 +0000

    [add]item controller

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,22 +1,45 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
     @items = Item.all
-    
+
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new
+    if @item.save
+      redirect_to :show
+    else
+      render :new
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
   end
+
+  private
+    def item_params
+      params.require(:item).permit(:image,:name,:introduction,:genre_id,:price,:is_active)
+    end
+  end
+
 
 end
