a8265d1003f106e1e8288d93605ca1edb21b6522
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-05-17 08:39:28 +0000

    [add]ad_login

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,22 +4,21 @@ class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
+  #def new
   #   super
-  # end
+  #end
 
   # POST /resource/sign_in
   # def create
   #   super
-  # end
+  #end
 
   # DELETE /resource/sign_out
-  # def destroy
+  #def destroy
   #   super
-  # end
+  #end
 
-  # protected
-
+  
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
