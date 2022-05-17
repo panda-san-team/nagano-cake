1d1d541b6f0e9fe9c6f0b1c98178511904db0c93
KanaKarimata <kana.lt22@gmail.com>
2022-05-17 02:48:10 +0000

    [Add]header(a logo and links)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "564c8222d86e4619911677e0d058e8cf.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
