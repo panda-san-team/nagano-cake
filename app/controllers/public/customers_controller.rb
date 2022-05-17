de61344fb848364448ad87d8927e891ef04b845a
KanaKarimata <kana.lt22@gmail.com>
2022-05-17 12:42:57 +0000

    [Add]my_page

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,18 @@
 class Public::CustomersController < ApplicationController
+  def show
+    @customer = current_customer 
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def quit
+  end
+  
+  def out
+  end
+  
 end
