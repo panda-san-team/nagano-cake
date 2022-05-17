ea747facb92706436b59d5f30756f1047cfaefa7
kao126 <76607249+kao126@users.noreply.github.com>
2022-05-17 12:46:33 +0000

    [Add]注文履歴一覧ページ,enum日本語Gem追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :addresses, dependent: :destroy
+  has_many :orders, dependent: :destroy
 
   enum is_deleted: { "有効": false, "退会": true }
 
