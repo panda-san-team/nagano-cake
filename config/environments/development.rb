d88244b439477ea6b0793226cb2e2199f9f94243
kao126 <76607249+kao126@users.noreply.github.com>
2022-05-17 05:06:05 +0000

    [Add]画面表示のためのconfig追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "938c1b9cf4d545269283c6f41f7d142b.vfs.cloud9.us-east-1.amazonaws.com"
 end
