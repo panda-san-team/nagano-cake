f2c02606e187c9e87f506bc6d472fbd74a8ceb38
kao126 <76607249+kao126@users.noreply.github.com>
2022-05-17 09:23:44 +0000

    [Add]会員一覧ページ（管理者）

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,9 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :addresses, dependent: :destroy
+
+  def full_name
+    self.last_name + self.first_name
+  end
+
 end
