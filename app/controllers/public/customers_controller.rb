d897cfaae2501d6c9519103547962a1a79367abe
KanaKarimata <kana.lt22@gmail.com>
2022-05-18 08:54:53 +0000

    [修正]edit_page また後で修正する

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -18,9 +18,9 @@ class Public::CustomersController < ApplicationController
 
   def out
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
   end
