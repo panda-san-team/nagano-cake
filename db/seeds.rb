9af511473d86d1775a3e8ad8ab1ef3e9dbe379ff
KanaKarimata <kana.lt22@gmail.com>
2022-05-19 23:41:33 +0000

    [Add]top_page images

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,48 @@ Admin.create!(
   email: 'admin@admin',
   password: 'testtest'
 )
+
+item = Item.create(
+  genre_id: 1,
+  name: 'チョコブラウニー',
+  introduction: 'クーベルチョコレートをたっぷり使ったちょっと贅沢したい時にピッタリのブラウニーです。',
+  price: 560,
+  is_active: true,
+  items_image: File.open("./app/assets/ブラウニー.jpeg")
+  )
+
+item = Item.create(
+  genre_id: 1,
+  name: 'レモンケーキ',
+  introduction: 'アールグレイの紅茶にとってもお似合いのレモンケーキをご用意しました。',
+  price: 400,
+  is_active: true,
+  items_image: File.open("./app/assets/レモンケーキ.jpeg")
+  )
+
+item = Item.create(
+  genre_id: 1,
+  name: 'いちごのホールケーキ',
+  introduction: 'あまおういちごをたっぷり使ったホールケーキです。',
+  price: 3600,
+  is_active: true,
+  items_image: File.open("./app/assets/いちごのホールケーキ.jpeg")
+  )
+
+item = Item.create(
+  genre_id: 1,
+  name: 'チョコレートカップケーキ',
+  introduction: '優雅なおやつの時間を過ごせます。',
+  price: 150,
+  is_active: true,
+  items_image: File.open("./app/assets/カップケーキ.jpeg")
+  )
+
+item = Item.create(
+  genre_id: 1,
+  name: 'いちごのホワイトロールケーキ',
+  introduction: '甘さ控えめのホワイトチョコがクセになる口当たりが軽いロールケーキです。',
+  price: 800,
+  is_active: true,
+  items_image: File.open("./app/assets/ロールケーキ.jpeg")
+  )
