289584539e4d815b9c324a75a781b882c95d0160
KanaKarimata <kana.lt22@gmail.com>
2022-05-19 03:21:11 +0000

    [Add]addresses_edit

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,22 +1,26 @@
 class Public::AddressesController < ApplicationController
   
   def index
+    @address = Address.new
     @customer = current_customer
-    @address = @customer.address.new
-    @addresses = @customer.addresses.all
+    @addresses = @customer.addresses
   end
   
   def create
-    @customer = current_customer
-    @address = @customer.address.new(customer_params)
-    @address.update
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
     redirect_to addresses_path
   end
   
   def edit
+    @address = Address.find(params[:id])
   end
   
   def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
   end
   
   def destroy
