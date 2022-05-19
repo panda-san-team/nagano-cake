c9dc1daa0a2fcf82883c505472d74436b8de1488
KanaKarimata <kana.lt22@gmail.com>
2022-05-19 03:08:09 +0000

    [Add]addresses_index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,25 @@
 class Public::AddressesController < ApplicationController
+  
+  def index
+    @customer = current_customer
+    @address = @customer.address.new
+    @addresses = @customer.addresses.all
+  end
+  
+  def create
+    @customer = current_customer
+    @address = @customer.address.new(customer_params)
+    @address.update
+    redirect_to addresses_path
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
 end
