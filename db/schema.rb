f20856170b13eb23465ae02ed0d56c2e19889d29
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-05-20 08:52:25 +0000

    [add]item_migration

--- db/schema.rb
+++ db/schema.rb
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 2022_05_18_121405) do
     t.string "name"
     t.text "introduction"
     t.integer "price"
-    t.boolean "is_active", default: true
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
