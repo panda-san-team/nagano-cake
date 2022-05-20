34ee5fdd0d36c7c80e55f003ad27b66b4464a280
kao126 <76607249+kao126@users.noreply.github.com>
2022-05-20 02:37:11 +0000

    [Update]注文一覧 デザイン/非同期通信化

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,9 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @orders = Order.all
+    @orders = Order.page(params[:page]).per(10)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
-  
 end
