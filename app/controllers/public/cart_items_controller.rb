04dca94a4e1d599c161742fbae40ea7d80c2503a
EC2 Default User <ec2-user@ip-172-31-73-197.ec2.internal>
2022-05-23 07:02:18 +0000

    なし

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -20,7 +20,7 @@ class Public::CartItemsController < ApplicationController
   end
 
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items.all
     @total_price = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
   end
 
