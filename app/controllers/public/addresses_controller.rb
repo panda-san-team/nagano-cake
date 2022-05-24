c53debf662fa50e74d74cdd9fc1f32ec155eb2b2
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-05-24 10:44:30 +0000

    [add]

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,35 +1,37 @@
 class Public::AddressesController < ApplicationController
-  
+
   def index
     @address = Address.new
     @customer = current_customer
     @addresses = @customer.addresses
   end
-  
+
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
     @address.save
     redirect_to addresses_path
   end
-  
+
   def edit
     @address = Address.find(params[:id])
   end
-  
+
   def update
     @address = Address.find(params[:id])
     @address.update(address_params)
     redirect_to addresses_path
   end
-  
+
   def destroy
+    Address.find(params[:id]).destroy
+    redirect_to request.referer
   end
-  
+
   private
-  
+
   def address_params
     params.require(:address).permit(:customer_id, :name, :postal_code, :address)
   end
-  
+
 end
