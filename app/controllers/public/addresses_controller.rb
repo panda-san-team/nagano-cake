2b88212820eed65f30d4add63efb90c01fe0bdc9
EC2 Default User <ec2-user@ip-172-31-73-197.ec2.internal>
2022-05-25 05:14:15 +0000

    昨日不具合修正

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -22,8 +22,11 @@ class Public::AddressesController < ApplicationController
 
   def update
     @address = Address.find(params[:id])
-    @address.update(address_params)
+    if @address.update(address_params)
     redirect_to addresses_path
+    else
+    render :edit
+    end
   end
 
   def destroy
@@ -34,7 +37,7 @@ class Public::AddressesController < ApplicationController
   private
 
   def address_params
-    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @items = Item.order('id DESC').limit(4)
+    @items = Item.where(is_active: true).order('id DESC').limit(4)
   end
 
   def about
